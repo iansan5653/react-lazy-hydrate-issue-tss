607ff9f2e7ee59f282d4ecc2c1bac5aabecb6083
Ian Sanders <iansan5653@gmail.com>
2026-02-25 14:06:38 -0500

    Add comment

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -6,8 +6,11 @@ export const Route = createFileRoute("/")({component: App});
 
 const LazyHelloWorld = lazy(async () => {
   console.log("Start lazy import");
+
   const module = await import("../components/HelloWorld");
+  // Artificially delay the import to make the fallback more obvious
   await new Promise((resolve) => setTimeout(resolve, 500));
+
   console.log("Finish lazy import");
   return module;
 });
